f1375fb87dcb5f96cbd23e75a4faff6564ac1cd9
Yes <joshuayes@storysensecomputing.com>
2014-09-27 09:43:56 +0800

    init commit

---
+++ app/src/main/java/tw/yes/androidskilltree/StartActivity.java
@@ -0,0 +1,14 @@
+package tw.yes.androidskilltree;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class StartActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
