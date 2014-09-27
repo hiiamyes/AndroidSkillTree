7c53aa04d837fa955c22d4e2012c0ce92837f64a
Yes <joshuayes@storysensecomputing.com>
2014-09-27 09:53:49 +0800

    Add AndroidAnnotation!

--- app/src/main/java/tw/yes/androidskilltree/StartActivity.java
+++ app/src/main/java/tw/yes/androidskilltree/StartActivity.java
@@ -1,14 +1,10 @@
 package tw.yes.androidskilltree;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
+import org.androidannotations.annotations.EActivity;
 
+@EActivity(R.layout.activity_start)
 public class StartActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
-    }
 }
